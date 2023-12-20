7ca065ca6fa69687d3e9e3086955d0c37e159c76
AlexMaltsev86 <149813897+AlexMaltsev86@users.noreply.github.com>
2023-12-20 18:57:33 +0300

    Update test_CloudApi.py

--- test_CloudApi.py
+++ test_CloudApi.py
@@ -6,5 +6,6 @@ client = TestClient(app)
 
 def test_read_CloudApi():
     response = client.post("/answer/", json={"question": "Вопрос", "search_topic": "Контекст"})
-    #assert response.status_code == 200
-    assert response.json() == {"answer": "Контекст"}
+    assert response.status_code == 200
+    result = response.json()
+    assert result['answer'] == "Контекст"
