74be82a158472b2c786590f82f24bd27bc96a277
AlexMaltsev86 <149813897+AlexMaltsev86@users.noreply.github.com>
2024-03-02 10:56:48 +0300

    Update evaStream.py

--- evaStream.py
+++ evaStream.py
@@ -1,16 +1,18 @@
 from transformers import pipeline
 import streamlit as st
 
+
 # Функция подгрузки данных модели
 @st.cache_resource
 def load_model():
-   model_pipeline = pipeline(model="AlexWortega/instruct_rugptlarge")
-   return model_pipeline
+    model_pipeline = pipeline(model="AlexWortega/instruct_rugptlarge")
+    return model_pipeline
+
 
 # Функция запуска модели
-def execute(question):
-   
-    gen_kwargs = {
+def execute(question):   
+
+   gen_kwargs = {
         "min_length": 20,
         "max_new_tokens": 100,
         "top_k": 50,
@@ -18,25 +20,26 @@ def execute(question):
         "do_sample": True,
         "early_stopping": True,
         "no_repeat_ngram_size": 2,
-        #"use_cache": True,
-        "repetition_penalty": 1.5
-        }
+        # "use_cache": True,
+        "repetition_penalty": 1.5,
+    }
     result = model(question, **gen_kwargs)
-    st.text('Ответ на вопрос: ' + result[0]['generated_text'])
+    st.text("Ответ на вопрос: " + result[0]["generated_text"])
+
 
 # Заголовок
-st.title(body= 'Приложение отвечающее на пользовательские вопросы')
+st.title(body='Приложение отвечающее на пользовательские вопросы')
 
 # Вопрос
-question = st.text_input(label='Введите вопрос',value=' ')
+question = st.text_input(label="Введите вопрос", value=" ")
 # Активность кнопки
-if question == ' ':
-   disabled = True
+if question == " ":
+    disabled = True
 else:
-   disabled = False
+    disabled = False
 # Подгрузка модели
 model = load_model()
 # Кнопка для запуска модели
-executed = st.button(label='Выполнить', disabled=disabled)
+executed = st.button(label="Выполнить", disabled=disabled)
 if executed:
-   execute(question)
+    execute(question)
