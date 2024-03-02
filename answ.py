f174b6c939a2dfe6e8679f5205e40cfb1e6c300b
AlexMaltsev86 <149813897+AlexMaltsev86@users.noreply.github.com>
2024-03-02 10:49:14 +0300

    Update answ.py

--- answ.py
+++ answ.py
@@ -1,28 +1,36 @@
 from transformers import pipeline
 import streamlit as st
+
+
 # Функция подгрузки данных модели
 @st.cache_resource
 def load_model():
-   model_pipeline = pipeline(task='question-answering', model='timpal0l/mdeberta-v3-base-squad2')
-   return model_pipeline
+    model_pipeline = pipeline(
+        task="question-answering", model="timpal0l/mdeberta-v3-base-squad2"
+    )
+    return model_pipeline
+
+
 # Функция запуска модели
-def execute(question1 , text1):
-   result = model(question=question1, context=text1)
-   st.text('Ответ на вопрос: ' + result['answer'])
+def execute(question1, text1):
+    result = model(question=question1, context=text1)
+    st.text("Ответ на вопрос: " + result["answer"])
+
+
 # Заголовок
-st.title(body= 'Приложение отвечающее на пользовательские вопросы по тексту')
+st.title(body="Приложение отвечающее на пользовательские вопросы по тексту")
 # Текст для анализа
-text = st.text_area(label='Введите текст',value=' ', height=300)
+text = st.text_area(label="Введите текст", value=" ", height=300)
 # Вопрос
-question = st.text_input(label='Введите вопрос',value=' ')
+question = st.text_input(label="Введите вопрос", value=" ")
 # Активность кнопки
-if text == ' ' or question == ' ':
-   disabled = True
+if text == " " or question == " ":
+    disabled = True
 else:
-   disabled = False
+    disabled = False
 # Подгрузка модели
 model = load_model()
 # Кнопка для запуска модели
-executed = st.button(label='Выполнить', disabled=disabled)
+executed = st.button(label="Выполнить", disabled=disabled)
 if executed:
-   execute(question, text)
+    execute(question, text)
