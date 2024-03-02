0b19f353ea5c736c7d64662189488152d0516be3
AlexMaltsev86 <149813897+AlexMaltsev86@users.noreply.github.com>
2024-03-02 10:50:45 +0300

    Update evaluation.py

--- evaluation.py
+++ evaluation.py
@@ -1,5 +1,5 @@
 from transformers import pipeline
 
 generator = pipeline(model="AlexWortega/instruct_rugptlarge")
-EvaluationDay = generator("Какой будет сегодня день?",return_full_text=True)
+EvaluationDay = generator("Какой будет сегодня день?", return_full_text=True)
 print(EvaluationDay)
