9b4386e18f89728d205324475dacff1555410108
AlexMaltsev86 <149813897+AlexMaltsev86@users.noreply.github.com>
2024-03-02 10:32:07 +0300

    Update CloudApi.py

--- CloudApi.py
+++ CloudApi.py
@@ -2,6 +2,7 @@ from transformers import pipeline
 from fastapi import FastAPI
 from pydantic import BaseModel
 
+
 # Функция подгрузки данных модели
 def load_model():
     model_pipeline = pipeline(
@@ -9,16 +10,19 @@ def load_model():
     )
     return model_pipeline
 
+
 # Класс для получения значения запрашиваемых параметров через API
 class ApiParams(BaseModel):
     search_topic: str
     question: str
+
 
 # Подгрузка модели
 model = load_model()
 
 # Объект для работы с api
 app = FastAPI()
+
 
 # Отправка результата запроса
 @app.post("/answer/")
