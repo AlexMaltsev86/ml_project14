d1ec31cc3d1952e6ee2e450c42ffa3ae9566ba62
AlexMaltsev86 <149813897+AlexMaltsev86@users.noreply.github.com>
2024-03-02 10:54:00 +0300

    Update test_CloudApi.py

--- test_CloudApi.py
+++ test_CloudApi.py
@@ -1,11 +1,13 @@
 from fastapi.testclient import TestClient
 from CloudApi import app
  
-allow_redirects=False
+allow_redirects = False
 client = TestClient(app)
 
 def test_read_CloudApi():
-    response = client.post("/answer/", json={"question": "Вопрос", "search_topic": "Контекст"})
+    response = client.post(
+        "/answer/", json={"question": "Вопрос", "search_topic": "Контекст"}
+    )
     assert response.status_code == 200
     result = response.json()
-    assert result['answer'] == "Контекст"
+    assert result["answer"] == "Контекст"
