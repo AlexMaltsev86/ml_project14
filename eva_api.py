e9ccb7776af1bf1603a28c8ecbccc9b3d6045b69
AlexMaltsev86 <149813897+AlexMaltsev86@users.noreply.github.com>
2024-03-02 10:52:45 +0300

    Update eva_api.py

--- eva_api.py
+++ eva_api.py
@@ -2,20 +2,24 @@ from transformers import pipeline
 from fastapi import FastAPI
 from pydantic import BaseModel
 
+
 # Функция подгрузки данных модели
 def load_model():
-   model_pipeline = pipeline(model="AlexWortega/instruct_rugptlarge")
-   return model_pipeline
+    model_pipeline = pipeline(model="AlexWortega/instruct_rugptlarge")
+    return model_pipeline
+
 
 # Класс для получения значения запрашиваемых параметров через API
 class ApiParams(BaseModel):
-   question: str
-   
+    question: str
+
+
 # Подгрузка модели
 model = load_model()
 
 # Объект для работы с api
 app = FastAPI()
+
 
 # Отправка результата запроса
 @app.post("/answer/")
@@ -28,9 +32,9 @@ def answer(params: ApiParams):
         "do_sample": True,
         "early_stopping": True,
         "no_repeat_ngram_size": 2,
-        #"use_cache": True,
-        "repetition_penalty": 1.5
-        }
+        # "use_cache": True,
+        "repetition_penalty": 1.5,
+    }
     result = model(params.question, **gen_kwargs)
 
-    return result[0]['generated_text']
+    return result[0]["generated_text"]
